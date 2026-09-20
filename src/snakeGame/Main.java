c86dd837e22e442c235be2c182c17b40cd9476e3
MidnightStellarrr <hannahgranada458@gmail.com>
2026-09-20 20:59:59 +0800

    added snake title

--- src/snakeGame/Main.java
+++ src/snakeGame/Main.java
@@ -1,13 +1,19 @@
 package snakeGame;
 
+import java.awt.Color;
 import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
     JFrame frame = new JFrame("Snake Game");
-    frame.setBounds(10,10,905,70);
+    //setBounds(x, y, width, height)
+    frame.setBounds(10,10,905,700);
     frame.setResizable(false);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    GamePanel panel = new GamePanel();
+    panel.setBackground(Color.DARK_GRAY);
+    frame.add(panel);
     frame.setVisible(true);
     }
 }
