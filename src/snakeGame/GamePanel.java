99f1463f58d58ddacd15b5a1b4f816ee6b714f01
MidnightStellarrr <hannahgranada458@gmail.com>
2026-09-22 23:24:44 +0800

    added partial positions in y and x

--- src/snakeGame/GamePanel.java
+++ src/snakeGame/GamePanel.java
@@ -5,6 +5,18 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
+    
+    private int[] snakexlength = new int[750];
+    private int[] snakeylength = new int[750];
+    private int lengthOfSnake = 3;
+    
+    private boolean left = false;
+    private boolean right = true;
+    private boolean up = false;
+    private boolean down = false;
+    
+    private int moves = 0;
+    
     private ImageIcon snakeTitle = new ImageIcon(getClass().getResource("snaketitle.jpg"));
     GamePanel(){
         
@@ -19,5 +31,20 @@ public class GamePanel extends JPanel{
         g.drawRect(24, 74, 851, 576);
         
         snakeTitle.paintIcon(this, g, 25, 11);
+        g.setColor(Color.black);
+        g.fillRect(25, 75, 850, 575);
+        
+        if(moves == 0){
+            snakexlength[0] = 100;
+            snakexlength[1] = 75;
+            snakexlength[2] = 50;
+            
+            snakeylength[0] = 100;
+            snakeylength[1] = 100;
+            snakeylength[2] = 100;
+            moves++;
+        }
+        
+        
     }
 }
